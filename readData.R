a367c776e31e9cbc9597e481513c1656cf646a31
Steven LaValle <steven.lavalle@gmail.com>
2015-04-25 22:02:10 -0400

    Creating repository

---
+++ readData.R
@@ -0,0 +1,5 @@
+readData <- function()  {
+  ## This first line will likely take a few seconds. Be patient!
+  NEI <- readRDS("summarySCC_PM25.rds")
+  SCC <- readRDS("Source_Classification_Code.rds")
+}
